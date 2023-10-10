67b4234b52ea7516433c4610b1c6bbf7dbfba48d
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-10-10 15:39:44 +0300

    Square to Circle Component

--- src/Componenets/GameBoard.js
+++ src/Componenets/GameBoard.js
@@ -3,31 +3,72 @@ import React from "react";
 import GameCircle from "./GameCircle"
 
 const GameBoard = () => {
+
+    const style = {
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr 1fr 1fr',
+        gridTemplateRows: '1fr 1fr 1fr 1fr',
+        padding: 20,
+        width: '500px',
+        position: 'absolute',
+        left: '50%',
+        marginLeft: '-265px',
+        top: '20%'
+
+    }
+
     return (
-        <div>
+        <div style={style}>
             <GameCircle id={1} color="red">
-                <span style={{ color: 'red' }}>Red</span>
+
             </GameCircle>
             <GameCircle id={2} color="blue">
-                <span style={{ color: 'blue' }}>Blue</span>
+
             </GameCircle>
             <GameCircle id={3} color="red">
-                <span style={{ color: 'red' }}>Red</span>
+
             </GameCircle>
-            <GameCircle id={4}>
-                <span style={{ color: 'blue' }}>Blue</span>
+            <GameCircle id={4} color="blue">
+
             </GameCircle>
-            <GameCircle id={5}>
-                <span style={{ color: 'red' }}>Red</span>
+            <GameCircle id={5} color="red">
+
             </GameCircle>
-            <GameCircle id={6}>
-                <span style={{ color: 'blue' }}>Blue</span>
+            <GameCircle id={6} color="blue">
+
             </GameCircle>
-            <GameCircle id={7}>
-                <span style={{ color: 'red' }}>Red</span>
+            <GameCircle id={7} color="red">
+
             </GameCircle>
-            <GameCircle id={8}>
-                <span style={{ color: 'blue' }}>Blue</span>
+            <GameCircle id={8} color="blue">
+
+            </GameCircle>
+            <GameCircle id={9} color="red">
+
+            </GameCircle>
+            <GameCircle id={10} color="blue">
+
+            </GameCircle>
+
+            <GameCircle id={11} color="red">
+
+            </GameCircle>
+            <GameCircle id={12} color="blue">
+
+            </GameCircle>
+
+            <GameCircle id={13} color="red">
+
+            </GameCircle>
+            <GameCircle id={14} color="blue">
+
+            </GameCircle>
+
+            <GameCircle id={15} color="red">
+
+            </GameCircle>
+            <GameCircle id={16} color="blue">
+
             </GameCircle>
         </div>
     )
--- src/Componenets/GameCircle.js
+++ src/Componenets/GameCircle.js
@@ -4,11 +4,18 @@ const onClick = (ev, id) => {
     alert('on click ' + id)
 }
 
-const GameCircle = ({id, color, children}) => {
-    console.log(id);
+const GameCircle = ({ id, color, children }) => {
+
+    const style = {
+        backgroundColor: color,
+        width: 100,
+        height: 100,
+        margin: 10,
+        borderRadius: '50%'
+    }
 
     return (
-        <div style={{ backgroundColor: color}} onClick={(ev) => onClick(ev, id)}>
+        <div style={style} onClick={(ev) => onClick(ev, id)}>
             {children}
         </div>
     )
