b267befba5e7554800dff95f205999e61df9e0d3
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-10-17 14:14:45 +0300

    Smarter Computer Player - Basic AI

--- src/helper.js
+++ src/helper.js
@@ -40,7 +40,7 @@ export const isDraw = (gameBoard, currentMove, currentPlayer) => {
     return count === 0;
 }
 
-export const getComputerMove = (gameBoard) => {
+const getRandomComputerMove = (gameBoard) => {
 
     let validMoves = [];
 
@@ -53,4 +53,69 @@ export const getComputerMove = (gameBoard) => {
     let randomMove = Math.floor(Math.random() * validMoves.length);
 
     return validMoves[randomMove];
+}
+
+const getPosition = (gameBoard, moveChecks) => {
+
+    for (let check = 0; check < moveChecks.length; check++) {
+        for (let i = 0; i < moveChecks[check].max; i += moveChecks[check.step]) {
+            let series = gameBoard[i + moveChecks[check].indexes[0]].toString() +
+                gameBoard[i + moveChecks[check].indexes[1]].toString() +
+                gameBoard[i + moveChecks[check].indexes[2]].toString() +
+                gameBoard[i + moveChecks[check].indexes[3]].toString();
+
+            switch (series) {
+                case "1110":
+                case "2220":
+                    return i + moveChecks[check].indexes[3];
+                case "1101":
+                case "2202":
+                    return i + moveChecks[check].indexes[2];
+                case "1011":
+                case "2022":
+                    return i + moveChecks[check].indexes[1];
+                case "0111":
+                case "0222":
+                    return i + moveChecks[check].indexes[0];
+                default:
+            }
+        }
+    }
+    return -1;
+}
+
+export const getComputerMove = (gameBoard) => {
+    let moveChecks = [
+
+        //* Vertical:
+        {
+            indexes: [0, 4, 8, 12],
+            max: 4,
+            step: 1,
+        },
+        //* Horizontal:
+        {
+            indexes: [0, 1, 2, 3],
+            max: 16,
+            step: 4,
+        },
+        //* Diagnoal:
+        {
+            indexes: [0, 5, 10, 15],
+            max: 16,
+            step: 16,
+        },
+        //* Diagnoal:
+        {
+            indexes: [3, 6, 9, 12],
+            max: 16,
+            step: 16,
+        },
+    ];
+
+    let position = getPosition(gameBoard, moveChecks);
+    if (position > -1) return position;
+
+    return getRandomComputerMove(gameBoard);
+
 }
